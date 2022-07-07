ca49bfc22679d79750d2a72b8f1484e47b2bc90e
Taraka Pavan <tarakapavanb@gmail.com>
2022-07-07 13:47:53 +1000

    updated menu styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Article from './Article';
-import logo from './logo.svg';
+
 import './index.css';
 
 
@@ -7,26 +7,12 @@ import './index.css';
 function App() {
   return (
    
-  <>
-    
-      <header className="App-header">
-      <div class="logo">
-          <img src={logo} className="App-logo" alt="logo" />
-          My First Application
-      </div>
-        <div class="menu">
-        <div class="box"> Menu Item 1 </div>
-        <div class="box"> Menu Item 1 </div>
-        <div class="box"> Menu Item 1 </div>
-        
-           </div>
-               
+ 
       
-      </header>
-      <body>
+ 
         <Article />
-      </body>
-    </>
+ 
+ 
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import logo from './logo.svg';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+   <header className="App-header">
+      <div className="logo">
+          <img src={logo} className="App-logo" alt="logo" />
+      </div>
+        <div className="menu">
+        <div className="menu-item"> Menu Item 1 </div>
+        <div className="menu-item"> Menu Item 2 </div>
+        <div className="menu-item"> Menu Item 3 </div>
+        
+           </div>
+               
+      
+      </header>
+      
+       <App />
+       
+       </>
   </React.StrictMode>
 );
 
